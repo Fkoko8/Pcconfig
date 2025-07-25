2fb822f4b25e5b53490bd032cc75f8641647dd61
Fkoko8 <147731702+Fkoko8@users.noreply.github.com>
2025-07-25 13:55:53 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Pcconfig/' : '/',
+  base: '/Pcconfig/',
   server: {
     host: "::",
     port: 8080,
